2da4222a082af2d25327d0853ce15d3e3656dbe6
Erick <erickkan2004@gmail.com>
2024-06-19 19:37:24 -0300

    foodie app start

---
+++ App.js
@@ -0,0 +1,13 @@
+import { Text } from 'react-native';
+import { styles } from "./style.js";
+import Button from "./src/components/button/button.jsx";
+
+export default function App() {
+
+  return (<>
+    <Text style={styles.textos}>Hello World</Text>
+
+    <Button texto="Acessar" />
+  </>
+  );
+}
